2e9e400c87d776264676c24ff8c8ebc13537575a
TarunZach <tzacharias.97@gmail.com>
2022-07-11 09:55:24 +0530

    ppt layout change

--- src/components/PDF.js
+++ src/components/PDF.js
@@ -1,8 +1,11 @@
 import React from "react";
 import "./css/pdf.css";
-import { Button, Table } from "react-bootstrap";
+import userimg from '../images/user.png'
+import work from '../images/work.png'
+import certificate from '../images/certification.png'
+import training from '../images/training.png'
 // import Table from "react-bootstrap/Table";
-import { useLocation } from "react-router-dom";
+import { useLocation, useNavigate } from "react-router-dom";
 import jsPDF from 'jspdf';
 
 import {
@@ -19,9 +22,9 @@ ChartJS.register(RadialLinearScale, ArcElement, Tooltip, Legend);
 
 
 const PDF = (props) => {
+  let history = useNavigate()
   const location = useLocation();
   const user = location.state;
-  const name = user.employee_name;
 
   const data = {
     labels: ['HTML', 'CSS', 'JavaScript', 'React'],
@@ -44,99 +47,97 @@ const PDF = (props) => {
   const generatePDF = () => {
     console.log('pdf button click');
     let doc = new jsPDF("l", "pt", "a3");
-    doc.html(document.getElementById('displayPDF'), { callback: function (pdf) { pdf.save(name) } });
+    doc.html(document.getElementById('displayPDF'), { callback: function (pdf) { pdf.save('candidate') } });
   }
   return (
-    <>
-      <div className="pdf">
-        <div className="content-wrapper">
-          <div className="Post" id='displayPDF'>
-            <header className="pdf-header">
-              <div className="item-wrapper">
-                <div className="user-image">
-                  <img
-                    src="http://demo.solwininfotech.com/wordpress/justica/wp-content/uploads/2016/07/Attorneys-5.png"
-                    alt="user"
-                    className="profile-img"
-                  />
+    user != null ?
+      <>
+        <div className="pdf">
+          <div className="content-wrapper">
+            <div className="Post" id='displayPDF'>
+              <header className="pdf-header">
+                <div className="item-wrapper">
+                  <div className="user-image">
+                    <img
+                      src={userimg}
+                      alt="user"
+                      className="profile-img"
+                    />
+                  </div>
+
+                  <p className="header-text">
+                    Name: {user.employee_name}
+                  </p>
+                  <p className="header-text">ID: {user.employee_id}</p>
+                  <p className="header-text">Grade: {user.employee_grade}</p>
+                  <p className="header-text">
+                    Experience: {user.employee_experience}
+                  </p>
+                </div>
+              </header>
+              <div className="user-details">
+
+                <div className="experience-table container experience">
+                  <div className="box">
+                    <img src={work} alt='experience' className="mini-img"/>
+                    <div className="heading">
+                      <p>Experience</p>
+                    </div>
+                    <div className="details">
+                      <p className="detail-text">
+                        {user.employee_primary_experience}
+                        {user.employee_secondary_experience}
+                      </p>
+                    </div>
+                  </div>
                 </div>
 
-                <p className="header-text">
-                  Employee Name: {user.employee_name}
-                </p>
-                <p className="header-text">Employee ID: {user.employee_id}</p>
-                <p className="header-text">Grade: {user.employee_grade}</p>
-                <p className="header-text">
-                  Experience: {user.employee_experience}
-                </p>
-              </div>
-            </header>
-            <div className="user-details">
-              <div className="first-row">
-                <div className="experience-table container">
-                  <Table striped bordered hover>
-                    <thead>
-                      <tr>
-                        <th>Experience</th>
-                      </tr>
-                    </thead>
-                    <tbody>
-                      <tr>
-                        <td colSpan={3}>
-                          {user.employee_primary_experience};
-                          {user.employee_secondary_experience}
-                        </td>
-                      </tr>
-                    </tbody>
-                  </Table>
+                <div className="experience-table container certification">
+                  <div className="box">
+                  <img src={certificate} alt='experience' className="mini-img"/>
+                    <div className="heading">
+                      <p >Certification and Achievements</p>
+                    </div>
+                    <div className="details">
+                      <p className="detail-text">
+                        {user.employee_certificates}</p>
+
+                    </div>
+                  </div>
                 </div>
 
-                <div className="experience-table container">
-                  <Table striped bordered hover>
-                    <thead>
-                      <tr>
-                        <th colSpan={3}>Certification and Achievements</th>
-                      </tr>
-                    </thead>
-                    <tbody>
-                      <tr>
-                        <td colSpan={3}>{user.employee_certificates}</td>
-                      </tr>
-                    </tbody>
-                  </Table>
-                </div>
-              </div>
-              <div className="second-row">
-                <div className="chart">
+                <div className="chart experience-table">
                   <PolarArea data={data} />
                 </div>
 
                 <div className="experience-table container training-table">
-                  <Table striped bordered hover>
-                    <thead>
-                      <tr>
-                        <th colSpan={3}>Trainings and Assessments</th>
-                      </tr>
-                    </thead>
-                    <tbody>
-                      <tr>
-                        <td colSpan={3}>{user.employee_trainings}</td>
-                      </tr>
-                    </tbody>
-                  </Table>
+                  <div className="box">
+                  <img src={training} alt='experience' className="mini-img"/>
+                    <div className="heading">
+                      <p >Trainings and Assessments</p>
+                    </div>
+                    <div className="details">
+                      <p className="detail-text">
+                        {user.employee_trainings}</p>
+                    </div>
+                  </div>
                 </div>
+
+
               </div>
-              <p>Reviews : testets</p>
+              <div className="review-container">
+                <p className="reviews">Reviews : testets</p>
+              </div>
             </div>
           </div>
-        </div>
-        <div className="generatePdf">
-          <button onClick={generatePDF} className="btn btn-primary" type="primary">Generate PDF</button>
-        </div>
-        <div id='displayPDF' />
+          <div className="generatePdf">
+            <button onClick={generatePDF} className="btn btn-primary" type="primary">Generate PDF</button>
+          </div>
+          <div id='displayPDF' />
 
-      </div>
-    </>
+        </div>
+      </>
+      : <button onClick={() => { history('/') }} className="btn btn-primary" type="primary">Go Back</button>
   );
 };
 
